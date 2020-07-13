782cd68df6f9b88197a044cf53f094d8cd29175b
coffeeandpouch <62453192+coffeeandpouch@users.noreply.github.com>
2020-07-13 10:05:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var STORE = {
         "c. Has been around ever since meat was cooked over an open flame",
         "d. Since Roman times",
       ],
-      answer: 2,
+      answer: "c. Has been around ever since meat was cooked over an open flame",
     },
     //2
     {
@@ -20,26 +20,26 @@ var STORE = {
         "c. New York, Deep Dish, and Detroit",
         "d. Texas, Carolina, Kansas City and Alabama",
       ],
-      answer: 3,
+      answer: "d. Texas, Carolina, Kansas City and Alabama" ,
     },
     //3
     {
       question: "Which style is know for using beef as the main meat source?",
       options: ["a.Carolina", "b.Alabama", "c.Texas", "d. Kansas City"],
-      answer: 2,
+      answer: "c.Texas",
     },
     //4
     {
       question:
         "Which region is famous for having 3 different styles of barbecue?",
       options: ["a.Alabama", "b.Carolina", "c.Texas", "d.Kansas City"],
-      answer: 1,
+      answer: "b.Carolina",
     },
     //5
     {
       question: "Which region is considered the crossroads of bbq?",
       options: ["a. Texas", "b. Alabama", "c. Kansas City", "d. Florida"],
-      answer: 2,
+      answer: "c. Kansas City",
     },
     //6
     {
@@ -50,24 +50,24 @@ var STORE = {
         "c. Pacific Northwest",
         "d. Homestyle",
       ],
-      answer: 0,
+      answer: "a. Alabama",
     },
     //7
     {
       question: "How is barbecue typically cooked?",
       options: [
         "a. Hot and Fast",
-        "b. COld and Quick",
+        "b. Cold and Quick",
         "c. Microwave",
         "d. Low and Slow",
       ],
-      answer: 3,
+      answer: "d. Low and Slow",
     },
     //8
     {
       question: "What temperature is most barbecue cooked to?",
       options: ["a. 145°F", "b. 32°F", "c. 203°C", "d. 203°F"],
-      answer: 3,
+      answer: "d. 203°F",
     },
     //9
     {
@@ -78,13 +78,13 @@ var STORE = {
         "c. Sous vide",
         "d. Flash fry",
       ],
-      answer: 1,
+      answer: "b. Indirect heat",
     },
     //10
     {
       question: "Which tool is most important for cooking barbecue?",
       options: ["a.Kitchen sink", "b. Grill/smoker", "c. Knife", "d. Spatula"],
-      answer: 1,
+      answer: "b. Grill/smoker",
     },
   ],
   correctAnswers: 0,
@@ -101,13 +101,14 @@ function populateOptions(currentQuestionOptions) {
     .join("");
 
 }
-//Shows and builds submit button 
+//Shows question and builds submit button
 function showQuestion(store) {
   $("#questions").html(`
-        <div>${STORE.questions[STORE.questionIndex].question}</div>
+        <div>Question #${store.questionIndex+1}</div>
+        <div>${store.questions[store.questionIndex].question}</div>
         <ul id="ul">
-            ${populateOptions(STORE.questions[store.questionIndex].options)}
-        </ul>     
+            ${populateOptions(store.questions[store.questionIndex].options)}
+        </ul>    
         <input type="submit" class="btnSubmit"></input>
     `);
 }
@@ -117,13 +118,22 @@ function toggleGreeting() {
 }
 //Provides visual feedback on whether question is correct or not
 function provideFeedback(store, element) {
-    if (element.val() == store.questions[store.questionIndex.answer]) {
+    if (element.val() == store.questions[store.questionIndex].answer) {
         store.correctAnswers++
         $("#questions").html(`<p>Correct!</p>`)
     } else {
-        $("#questions").html(`<p>Wrong!</p>`) 
+        $("#questions").html(`<p>Wrong!</p>
+        <div>The correct answer is:<br>
+        ${STORE.questions[STORE.questionIndex].answer}</div>`)
+
     }
 }
+//Computes the score at the end of the quiz
+function showScore(store) {
+  $("#questions").html(`
+            <div>Score:${store.correctAnswers}/${store.questions.length}</div>`)
+}
+
 //Function that starts the quiz and show the first question
 $(function () {
   $("#start").click(function (event) {
@@ -131,39 +141,40 @@ $(function () {
     toggleGreeting();
     showQuestion(STORE);
   });
-//Submits user answer input
+
+  //Submits user answer input and provides feedback
   $("#questions").submit(function(event) {
     event.preventDefault();
-    console.log('submitted');
-    STORE.questionIndex++;
-
     $(".btnSubmit").hide();
     var answer_element = $("input[name='answer']:checked")
     provideFeedback(STORE, answer_element);
-    $("#questions").after(`<div><button class="btnNext" id="next">
-                Next</button></div>`)
-
-    
-
-
+    STORE.questionIndex++;
+    if (STORE.questionIndex < 10) {
+      $('#next').toggle();
+    } else {
+      $('#score').toggle();
+    }
   });
-
-    // Move button to html after question form 
-    // Make next button hidden to start 
-    // Show the button when question answer submitted 
-
-
-    // Move showQuestion to the action triggered by the next button 
-    // showQuestion(STORE);
-    // Hide the next button 
-  $('#next').click(function (event) {  
-    event.PreventDefault();
-    showQuestion();
-    $("#next").hide();
-
-
+  //What happens when the next button is clicked
+  $('#next').click(function (event) {
+    event.preventDefault();
+      $('#next').toggle();
+      showQuestion(STORE);
   });
-)
-
-
-
+  //When the show score button at the end of the quiz is clicked and generates restart button
+  $('#score').click(function (event) {
+    event.preventDefault();
+      $('#score').toggle();
+      showScore(STORE);
+      $('#restart').toggle();
+  });
+  //Shows what happens when the restart button is clicked
+  $('#restart').click(function (event) {
+    event.preventDefault();
+    STORE.questionIndex = 0;
+    STORE.correctAnswers = 0; 
+    $('#restart').toggle();
+    showQuestion(STORE);
+  });
+  
+});
